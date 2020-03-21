132175eb6f2cf419f8435df8a2c7f845fa4e5a60
Jesús Hernández <jsabdiel.hm@gmail.com>
2020-03-21 00:19:19 -0600

    Se asignó la variable greetings y se hizo un análisis descriptivo a df

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
-print('Hello World!')
+#greeting = 'Hello World!'
+
+#print(greeting)
 
 import pandas as pd
 
 df = pd.DataFrame({'x':[1, 2, 3, 3.5, 9], 'y':[9, 4, 54, 5.5, 0]})
 
 df.head()
+
+df.describe()
